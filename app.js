007160ec4bc698e22b7436bc99779394a263525a
aregchatalian <aregchatalian@gmail.com>
2022-03-05 10:45:00 +0000

    git

--- app.js
+++ app.js
@@ -23,14 +23,14 @@ if (process.env.NODE_ENV === 'production') {
 
   app.get('/pull', (req, res) => {
     exec('git pull', (error, stdout, stderr) => {
-      if (error) return console.error(error);
-      console.log(stdout);
+      if (error) return console.error('Git pull failed.');
     });
 
-    setTimeout(() => {exec('pm2 reload 0')}, 3000);
-    setTimeout(() => {exec('pm2 restart 0')}, 4000);
-
-    res.send('Pulling...');
+    if (stdout !== 'Already up to date.') {
+      setTimeout(() => {exec('pm2 reload 0')}, 3000);
+      setTimeout(() => {exec('pm2 restart 0')}, 4000);
+      res.send('Pulling..., Server is rebooting.');
+    }
   });
 
   app.get('*', (req, res) => {
