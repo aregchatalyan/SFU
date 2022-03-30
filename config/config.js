96cca0c3ee4d314941f4d1964959085dc2a571b8
MelqonHovhannisyan <melkon.hovhannisian@gmail.com>
2022-03-30 18:12:34 +0400

    codecs

--- config/config.js
+++ config/config.js
@@ -37,18 +37,38 @@ module.exports = {
     router: {
       mediaCodecs: [
         {
-          kind: "audio",
-          mimeType: "audio/opus",
+          kind: 'audio',
+          mimeType: 'audio/opus',
           clockRate: 48000,
-          channels: 2,
+          channels: 2
         },
         {
-          kind: "video",
-          mimeType: "video/H264",
+          kind: 'video',
+          mimeType: 'video/VP8',
           clockRate: 90000,
           parameters: {
-            "x-google-start-bitrate": 500,
-          },
+            'x-google-start-bitrate': 1000
+          }
+        },
+        {
+          kind: 'video',
+          mimeType: 'video/VP9',
+          clockRate: 90000,
+          parameters: {
+            'profile-id': 2,
+            'x-google-start-bitrate': 1000
+          }
+        },
+        {
+          kind: 'video',
+          mimeType: 'video/H264',
+          clockRate: 90000,
+          parameters: {
+            'packetization-mode': 1,
+            'profile-level-id': '42e01f',
+            'level-asymmetry-allowed': 1,
+            'x-google-start-bitrate': 1000
+          }
         },
       ],
     },
