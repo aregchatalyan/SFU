73ed2aa915a1cf0e2c6b331deeedbfdc2a79881a
melkon98 <melkon.hovhannisian@gmail.com>
2022-06-13 17:26:54 +0400

    url token update3

--- view/src/containers/App/index.jsx
+++ view/src/containers/App/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Error from '../../components/pages/Error'
 import App from './App'
 
 const Container = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/:roomId" component={App}/>
+      <Route path="/" exact component={Error}/>
     </Switch>
   </BrowserRouter>
 )
